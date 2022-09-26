257b7137ff9e09c3f0d2b813ba4ea103c189c96d
Yasmin-Akhter <yasminbd89@yahoo.com>
2022-09-26 17:12:53 +0600

    product part working

--- pages/products.js
+++ pages/products.js
@@ -5,21 +5,38 @@ import Products1 from '../components/Products/Products1';
 import Products2 from '../components/Products/Products2';
 import ProductSlider from '../components/Products/ProductSlider';
 import FooterCommon from '../components/FooterCommon/FooterCommon';
+import axios from 'axios';
 
-const products = () => {
+const products = ({ productBannerData,product1Data,product2ndData }) => {
     return (
         <div style={{
             color: "rgb(23, 52, 101)",
             fontFamily: 'Poppins'
         }}>
             
-            <ProductBanner />
-            <Products1 />
-            <Products2 />
+            <ProductBanner productBannerData={productBannerData} />
+            <Products1 product1Data={product1Data} />
+            <Products2 product2ndData={product2ndData} />
             <ProductSlider />
           
         </div>
     );
 };
-
 export default products;
+
+export async function getServerSideProps() {
+    const productBannerData = await axios.get('http://localhost:3000/api/products/productbannerapi')
+    const product1Data = await axios.get('http://localhost:3000/api/products/product1api')
+    const product2ndData = await axios.get('http://localhost:3000/api/products/product2ndapi')
+    
+
+
+    return {
+        props: {
+            productBannerData: productBannerData.data,   
+            product1Data: product1Data.data,   
+            product2ndData: product2ndData.data,   
+        }
+    }
+
+}
---
+++ public/JSON/products/product1Json.js
@@ -0,0 +1,18 @@
+export const product1stJson = {
+    "id": 1,
+    "isShow": true,
+    "items": [
+        {
+            "id": 1,
+            "isShow": true,
+            "title": "We Have More Than 20+ Years Practical Experience",
+            "titleDescription": "On the other hand we denounce with righteous dislike men who are so beguiled.",
+            "subTitle1": "Protect your Data and Privacy",
+            "subTitleDescription1": "On the other hand we denounce with righteus indignation dislike men who are beguiled ",
+            "subTitle2": "Free And Open Source Software",
+            "subTitleDescription2": "On the other hand we denounce with righteus indignation dislike men who are beguiled",
+            "img": "/download (2).jpg",
+        }
+    ],
+
+}
