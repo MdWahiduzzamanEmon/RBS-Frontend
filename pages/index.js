e8d0bb61200e79652e279d7317d892fdf5e03721
Yasmin-Akhter <yasminbd89@yahoo.com>
2022-09-26 13:52:39 +0600

    banner updated

--- pages/index.js
+++ pages/index.js
@@ -50,21 +50,15 @@ export async function getServerSideProps(){
   const bannerImages= await axios.get('http://localhost:3000/api/bannerImage')
   const home2ndData = await axios.get('http://localhost:3000/api/home2ndapi')
   const home3rdInfos= await axios.get('http://localhost:3000/api/home3rdapi')
-  const home5thInfos= await axios.get('http://localhost:3000/api/home5thapi')
-  const home7thInfos= await axios.get('http://localhost:3000/api/home7thapi')
   const home4thData = await axios.get('http://localhost:3000/api/home4thapi')
+  const home5thInfos= await axios.get('http://localhost:3000/api/home5thapi')
   const home6thData = await axios.get('http://localhost:3000/api/home6thapi')
+  const home7thInfos= await axios.get('http://localhost:3000/api/home7thapi')
   const home8thData = await axios.get('http://localhost:3000/api/home8thapi')
   const home9thData = await axios.get('http://localhost:3000/api/home9thapi')
   const home10thData = await axios.get('http://localhost:3000/api/home10thapi')
   const purchaseCardInfos = await axios.get('http://localhost:3000/api/purchaseCard')
-  // const footerInfos= await axios.get('http://localhost:3000/api/footerapi')
-  // const home11thData = await axios.get('http://localhost:3000/api/home11thapi')
-
-  // const newData = await response.json()
-  // console.log(response.data)
-  // console.log(response)
-  console.log(home4thData)
+  
 
   return {
     props: {
