5a4c01410bd30542945708900ba643d99b48371d
Md. Thowhidul Islam Molla <thowhidulislam52@gmail.com>
2022-11-27 19:03:57 +0600

    tutorial steps and image description added

--- pages/allFeatures/[name].js
+++ pages/allFeatures/[name].js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link.js";
 import React from "react";
+import { Accordion } from "react-bootstrap";
 import { allFeaturesDemoData } from "../../public/JSON/allFeatures/allFeatures";
 import styles from "./SingleFeature.module.css";
 
@@ -27,13 +28,14 @@ export const getStaticProps = async ({ params }) => {
 };
 
 const SingleFeature = ({ feature }) => {
-  //   console.log(allFeaturesDemoData);
   console.log(feature);
+  const [stepId, setStepId] = React.useState(0);
+  console.log(stepId);
   return (
     <div className="container-xl">
       {feature.map((item) => (
         <div key={item.id}>
-          {/* <h1 className={`${styles.title}`}>{item.details.title}</h1> */}
+          {/* banner */}
           <div
             className={`${styles.bannerContainer} flex-md-column flex-sm-column flex-lg-row flex-column align-items-center justify-content-center`}
           >
@@ -41,7 +43,15 @@ const SingleFeature = ({ feature }) => {
               className={`${styles.bannerInnerContainerTop} p-5 text-justify`}
             >
               <p className={`${styles.bannerInnerContainerPath}`}>
-                Features/{item.banner.title}
+                <Link href="/allFeatures">
+                  <span className={`${styles.bannerInnerContainerPathLeft}`}>
+                    Features
+                  </span>
+                </Link>
+                <span style={{ color: "#888fb1" }}> / </span>
+                <span className={`${styles.bannerInnerContainerPathRight}`}>
+                  {item.banner.title}
+                </span>
               </p>
               <h3 className={`${styles.bannerInnerContainerTitle}`}>
                 {item.banner.title}
@@ -56,11 +66,116 @@ const SingleFeature = ({ feature }) => {
                 loop
                 muted
                 src={item?.banner?.video}
-                // layout="responsive"
-                width="600"
-                height="auto"
-                // className={`${styles.bannerInnerContainerVideo}`}
+                className={`${styles.bannerInnerContainerVideo}`}
               ></video>
+            </div>
+          </div>
+          {/* large image with features in two columns */}
+          <div className={`${styles.largeImageWithFeatures}`}>
+            <div className={`${styles.largeImageWithFeaturesTitleContainer}`}>
+              <h3 className={`${styles.largeImageWithFeaturesTitle}`}>
+                {item.details.title}
+              </h3>
+              <p className={`${styles.largeImageWithFeaturesInfos}`}>
+                {item.details.description}
+              </p>
+            </div>
+            <div className={`${styles.largeImageWithFeaturesImage}`}>
+              {item.details.image && (
+                <Image
+                  src={item?.details?.image}
+                  alt=""
+                  width={1200}
+                  height={600}
+                ></Image>
+              )}
+            </div>
+            <div className={`${styles.largeImageWithFeaturesList}`}>
+              {item.details?.featuresDetails?.map((sFeature) => (
+                <div key={sFeature.id}>
+                  <h5 className={`${styles.largeImageWithFeaturesListTitle}`}>
+                    {sFeature.title}
+                  </h5>
+                  <p className={`${styles.largeImageWithFeaturesListDetails}`}>
+                    {sFeature.description}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* accordion with image ---tutorials steps */}
+          <div className={`${styles.tutorialsContainer} p-5`}>
+            <h3 className={`${styles.tutorialsContainerTitle}`}>
+              {item.tutorials.title}
+            </h3>
+            <div className={`${styles.tutorialsStepsContainer}`}>
+              <div className={`${styles.tutorialsStepsAccordionContainer}`}>
+                <Accordion
+                  defaultActiveKey="1"
+                  flush
+                  className={`${styles.tutorialsStepsAccordion}`}
+                >
+                  {item.tutorials.steps.map((step) => (
+                    <Accordion.Item
+                      eventKey={`${step.id}`}
+                      key={step.id}
+                      className={`${styles.tutorialsStepsAccordionItem}`}
+                      onClick={() => setStepId(step.id)}
+                    >
+                      <Accordion.Header
+                        className={`${styles.tutorialsStepsTitle}`}
+                      >
+                        {step.title}
+                      </Accordion.Header>
+                      <Accordion.Body
+                        className={`${styles.tutorialsStepsDescription}`}
+                      >
+                        {step.description}
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  ))}
+                  {/* <Accordion.Item eventKey="1">
+                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Body>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </Accordion.Body>
+                  </Accordion.Item> */}
+                </Accordion>
+              </div>
+              <div className={`${styles.tutorialsImageContainer}`}>
+                {item.tutorials.steps[0].image && !stepId && (
+                  <Image
+                    src={item.tutorials.steps[0].image}
+                    alt=""
+                    width={500}
+                    height={500}
+                    className={`${styles.tutorialsImage}`}
+                  ></Image>
+                )}
+                <>
+                  {item.tutorials.steps.map((sImage) => (
+                    <>
+                      {sImage.id === stepId && (
+                        <Image
+                          key={sImage.id}
+                          src={sImage.image}
+                          alt=""
+                          width={500}
+                          height={500}
+                          className={`${styles.tutorialsImage}`}
+                        ></Image>
+                      )}
+                    </>
+                  ))}
+                </>
+              </div>
             </div>
           </div>
         </div>
--- public/JSON/allFeatures/allFeatures.js
+++ public/JSON/allFeatures/allFeatures.js
@@ -11,8 +11,8 @@ export const allFeaturesDemoData = [
     details: {
       title: "White Label Branding",
       description:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam id deserunt modi illum error fugit.",
-      image: "data",
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam id deserunt modi illum error fugit.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur architecto quo magni. Odio, voluptatem harum.",
+      image: "/pageImages/pageImage1.png",
       featuresDetails: [
         {
           id: 1,
@@ -40,12 +40,47 @@ export const allFeaturesDemoData = [
         },
       ],
     },
+    tutorials: {
+      id: 1,
+      title: "How to use white label branding",
+      steps: [
+        {
+          id: 1,
+          title:
+            "Step 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, blanditiis!",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dignissimos.",
+          image: "/pageImages/pageImage5.png",
+        },
+        {
+          id: 2,
+          title: "Step 2",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dignissimos.",
+          image: "/pageImages/pageImage4.png",
+        },
+        {
+          id: 3,
+          title: "Step 3",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dignissimos.",
+          image: "/pageImages/pageImage5.png",
+        },
+        {
+          id: 4,
+          title: "Step 4",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dignissimos.",
+          image: "/pageImages/pageImage4.png",
+        },
+      ],
+    },
   },
   {
     id: 2,
     name: "Kanban-Board",
     banner: {
-      image: "data",
+      // image: "data",
       title: "Kanban Board",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl lorem quis nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl lorem quis nisl.",
@@ -54,7 +89,7 @@ export const allFeaturesDemoData = [
       title: "Kanban Board",
       description:
         "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam id deserunt modi illum error fugit.",
-      image: "data",
+      // image: "data",
       featuresDetails: [
         {
           id: 1,
