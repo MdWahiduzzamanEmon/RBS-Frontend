e9d0e7fa85088de909fdce6525cda3c3edebf482
Md. Thowhidul Islam Molla <thowhidulislam52@gmail.com>
2023-01-22 14:48:04 +0600

    updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "14168a604a183b4e"; }
+/******/ 		__webpack_require__.h = function() { return "a707e8b84d6cd85a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
